31bd8365b70f79aac6667d3bd58b64fd38898634
Talha Ali <123146257+talhaali4722@users.noreply.github.com>
2023-05-05 09:51:11 +0500

    test from

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FormTest from './Component/FormTest';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+ <FormTest/>
+    </div>
+  );
+}
+
+export default App;
